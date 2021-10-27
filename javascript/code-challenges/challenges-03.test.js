e5929c2b4b99f97c4ffe87ddb8fb347ee0c4f1ab
Joe Streifel <josephstreifel@gmail.com>
2021-10-27 14:44:13 -0700

    completes challenge 7

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -116,7 +116,8 @@ For example, getStatName(snorlaxData.stats, 50) will return ['special-defense', 
 ------------------------------------------------------------------------------------------------ */
 
 const getStatName = (arr, minBaseStat) => {
-  // Solution code here...
+  let filteredStats = arr.filter(data => data.baseStat > minBaseStat);
+  return filteredStats.map(data => data.stat.name);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -263,7 +264,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return the name of the stats that exceed that maximum', () => {
     expect(getStatName(snorlaxData.stats, 50)).toStrictEqual([ 'special-defense', 'special-attack' ]);
     expect(getStatName(snorlaxData.stats, 50).length).toStrictEqual(2);
