dd2086465aec89964df65bfa68a14b83f01ebfdd
Joe Streifel <josephstreifel@gmail.com>
2021-10-28 15:57:22 -0700

    completes challenge 9

--- javascript/code-challenges/challenges-04.test.js
+++ javascript/code-challenges/challenges-04.test.js
@@ -137,7 +137,13 @@ For example, [1, 14, 0.2, -281, 54782] is only correctly sorted in that order.
 ------------------------------------------------------------------------------------------------ */
 
 const sortNumbersByLength = (arr) => {
-  // Solution code here...
+  const mapped = arr.map((num, i) => {
+    return {i, value: num.toString()};
+  });
+
+  mapped.sort((a, b) => a.value.length - b.value.length);
+
+  return mapped.map((obj => arr[obj.i]));
 };
 
 /*-----------------------------------------------------------------------------------------------
@@ -306,7 +312,7 @@ describe('Testing challenge 8', () => {
   });
 });
 
-xdescribe('Testing challenge 9', () => {
+describe('Testing challenge 9', () => {
   test('It should sort numbers by their length', () => {
     expect(sortNumbersByLength([10, 2.8, 1, -47.75])).toStrictEqual([1, 10, 2.8, -47.75]);
     expect(sortNumbersByLength([100, 2.82, 1, -47.75])).toStrictEqual([1, 100, 2.82, -47.75]);
@@ -314,7 +320,7 @@ xdescribe('Testing challenge 9', () => {
   });
 });
 
-xdescribe('Testing challenge 10', () => {
+describe('Testing challenge 10', () => {
   test('It should sort people by their last names', () => {
     expect(sortPeople(people)).toStrictEqual([
       new Person('Casey', 'Codefellow', 38),
