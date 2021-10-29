c2407134fc6912d0247cb18da3b14ecffd8cb364
Joe Streifel <josephstreifel@gmail.com>
2021-10-29 14:59:58 -0700

    completes challenge 3

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -40,7 +40,7 @@ Write a function named addPurchases that, given an array of objects as input, us
 ------------------------------------------------------------------------------------------------ */
 
 const addPurchases = (arr) => {
-  // Solution code here...
+  return arr.reduce((acc, val) => acc + val.purchasePrice, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -298,7 +298,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should add the purchase price', () => {
     expect(addPurchases([{item: 'switch', purchasePrice: 399}, {item: 'toothpaste', purchasePrice: 2}])).toStrictEqual(401);
     expect(addPurchases([])).toStrictEqual(0);
