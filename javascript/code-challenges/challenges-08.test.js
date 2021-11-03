b0df967455fdf37c948d21f32e49f6502ddaa4e0
Joe Streifel <josephstreifel@gmail.com>
2021-11-03 15:02:17 -0700

    completes challenge 5

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -107,7 +107,7 @@ Return an array containing all the matches.
 ------------------------------------------------------------------------------------------------ */
 
 const isCapitalized = (str) => {
-  // Solution code here...
+  return str.match(/[A-Z][\w]+/g) || [] ;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -238,7 +238,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should only return words that begin with a capital letter', () => {
     const capitalResult = isCapitalized('We only want to Return the Words that begin With a capital Letter');
 
