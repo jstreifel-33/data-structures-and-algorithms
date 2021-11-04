7fd3c85590b71c82fd74be18176b9bfd47a92291
Joe Streifel <josephstreifel@gmail.com>
2021-11-04 13:52:09 -0700

    completes challenge 4

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -60,7 +60,7 @@ HR has asked you to change the data to make it easier to print so that it looks 
 ------------------------------------------------------------------------------------------------ */
 
 const updateNumbers = (obj) => {
-  // Solution code here...
+  return Object.entries(obj).map(entry => entry[0] + ': ' + entry[1]);
 };
 
 
@@ -235,7 +235,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should return an an array of names and numbers', () => {
     const startingObj = {
       'Grace Hopper': '222-303-5938',
