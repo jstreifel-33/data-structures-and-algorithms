6283ac87492c6789d7fe30a81f2ef21a4dbd8be4
Joe Streifel <josephstreifel@gmail.com>
2021-11-10 22:13:16 -0800

    completes challenge 11

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -150,7 +150,7 @@ For example, ['abcd', 'efgh', 'ijkl', 'mnop'] returns ['a', 'f', 'k', 'p']
 ------------------------------------------------------------------------------------------------ */
 
 const characterByIndex = (arr) => {
-  // Solution code here...
+  return arr.map((str, idx) => str.charAt(idx));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -286,7 +286,7 @@ describe('Testing challenge 10', () => {
   });
 });
 
-xdescribe('Testing challenge 11', () => {
+describe('Testing challenge 11', () => {
   test('It should return the ith character of the ith string', () => {
     const words = ['apple', 'banana', 'cantaloupe'];
 
