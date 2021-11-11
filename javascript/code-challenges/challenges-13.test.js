265a97b6e95efc66e89732ac66aa577e7f503429
Joe Streifel <josephstreifel@gmail.com>
2021-11-10 20:43:32 -0800

    completes challenge 7

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -85,7 +85,7 @@ Write a function named findAnything that takes in an array of strings, along wit
 ------------------------------------------------------------------------------------------------ */
 
 const findAnything = (arr, target) => {
-  // Solution code here...
+  return arr.filter(str => str.includes(target));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -225,7 +225,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should find all the strings that contain a given string', () => {
     const words = ['things', 'apple (:)', ':)banana', 'missing that thing', 'cant:)aloupe'];
 
