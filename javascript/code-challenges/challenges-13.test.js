a3be3d3cf384101e8d9d47d93e8e249d767e8596
Joe Streifel <josephstreifel@gmail.com>
2021-11-10 20:27:56 -0800

    completes challenge 4

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -47,7 +47,7 @@ For example, (123) 456-7890 returns 1234567890
 ------------------------------------------------------------------------------------------------ */
 
 const standardizePhoneNumbers = (arr) => {
-  // Solution code here...
+  return arr.map(str => str.replaceAll(/\D/g, ''));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -191,7 +191,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should return a standardized set of phone numbers', () => {
     const nums = ['(123) 456-7890', '(222) 222-2222'];
 
