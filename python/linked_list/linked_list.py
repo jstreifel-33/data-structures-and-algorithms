5e8110514994fc39d9f86641d213882825986943
Joe Streifel <josephstreifel@gmail.com>
2021-12-05 15:13:07 -0800

    sets up Node class

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,3 +1,8 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
 class LinkedList:
     """
     Put docstring here
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,15 @@
-from linked_list.linked_list import LinkedList
+from linked_list.linked_list import LinkedList, Node
 
 
 def test_import():
     assert LinkedList
+
+
+def test_linked_list_instantiation():
+    assert LinkedList()
+
+
+def test_node_instantiation():
+    breakfast = Node('spam')
+    assert breakfast.value == 'spam'
+    assert breakfast.next is None
