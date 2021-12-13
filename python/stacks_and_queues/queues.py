350e948a9ca2d0e306cda2c608667e80d989b01a
Joe Streifel <josephstreifel@gmail.com>
2021-12-12 20:47:02 -0800

    initial setup for implementing stacks and queues

---
+++ python/stacks_and_queues/queues.py
@@ -0,0 +1,8 @@
+from stacks_and_queues.node import Node
+
+class InvalidOperationError(Exception):
+    pass
+
+class Queue:
+    pass
+
---
+++ python/stacks_and_queues/stacks.py
@@ -0,0 +1,7 @@
+from stacks_and_queues.node import Node
+
+class InvalidOperationError(Exception):
+    pass
+
+class Stack:
+    pass
