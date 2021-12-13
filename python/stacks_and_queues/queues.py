f595a1cb419daabb36eab359de3214a2140ebf25
Joe Streifel <josephstreifel@gmail.com>
2021-12-12 21:10:05 -0800

    implements Queue enqueue and dequeue methods

--- python/stacks_and_queues/queues.py
+++ python/stacks_and_queues/queues.py
@@ -4,5 +4,17 @@ class InvalidOperationError(Exception):
     pass
 
 class Queue:
-    pass
+    def __init__(self) -> None:
+        self.front = None
+
+    def enqueue(self, value):
+        if self.front is None:
+            self.front = Node(value)
+        else:
+            self.front.next = Node(value)
+
+    def dequeue(self):
+        front_val = self.front.value
+        self.front = self.front.next
+        return front_val
 
