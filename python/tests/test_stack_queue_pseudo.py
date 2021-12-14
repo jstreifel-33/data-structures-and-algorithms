5fdc4ad4d583700ad6429b1bb2a7ba02a9dd0e0d
Joe Streifel <josephstreifel@gmail.com>
2021-12-13 21:07:57 -0800

    tests multiple operations and exception raising

--- python/tests/test_stack_queue_pseudo.py
+++ python/tests/test_stack_queue_pseudo.py
@@ -13,3 +13,33 @@ def test_single_value():
     actual = q.dequeue()
     expected = 'spam'
     assert actual == expected
+
+
+def test_many_values():
+    q = PseudoQueue()
+    q.enqueue('spam')
+    q.enqueue('eggs')
+    q.enqueue('sparrow')
+    assert q.dequeue() == 'spam'
+    assert q.dequeue() == 'eggs'
+    assert q.dequeue() == 'sparrow'
+
+
+def test_dequeue_empty_exception():
+    q = PseudoQueue()
+    with pytest.raises(InvalidOperationError) as e:
+        q.dequeue()
+
+    assert str(e.value) == 'Operation cannot be performed on empty collection'
+
+
+def test_dequeue_out_of_values():
+    q = PseudoQueue()
+    q.enqueue('spam')
+    q.enqueue('eggs')
+    q.dequeue()
+    q.dequeue()
+    with pytest.raises(InvalidOperationError) as e:
+        q.dequeue()
+
+    assert str(e.value) == 'Operation cannot be performed on empty collection'
