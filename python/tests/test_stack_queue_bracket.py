d5066025e1977368f3becf871b88d47a0f7b6cd0
Joe Streifel <josephstreifel@gmail.com>
2021-12-15 22:06:09 -0800

    tests basic passing and failing strings

--- python/tests/test_stack_queue_bracket.py
+++ python/tests/test_stack_queue_bracket.py
@@ -1,1 +1,16 @@
 import pytest
+from code_challenges.stack_queue_brackets.stack_queue_bracket import validate_brackets
+
+
+def test_passing_string():
+    actual = validate_brackets("(){[]}")
+    expected = True
+    assert actual == expected
+
+
+def test_failing_string():
+    actual = validate_brackets("(){[}]")
+    expected = False
+    assert actual == expected
+
+
