5fa58d94e2713677746bb1e2167b3bfab519dd92
Joe Streifel <josephstreifel@gmail.com>
2021-12-19 18:13:29 -0800

    implements three traversal methods

--- python/tests/test_trees.py
+++ python/tests/test_trees.py
@@ -1,10 +1,8 @@
 from trees.trees import BinaryTree, BinarySearchTree, Node
+import pytest
 
 
 # Feature Tests:
-# Can successfully return a collection from a preorder traversal
-# Can successfully return a collection from an inorder traversal
-# Can successfully return a collection from a postorder traversal
 
 # Can successfully instantiate an empty tree
 def test_tree_instance():
@@ -24,3 +22,30 @@ def test_tree_left_right():
     assert tree.root.value == "spam"
     assert tree.root.left.value == "eggs"
     assert tree.root.right.value == "hash"
+
+# Can successfully return a collection from a preorder traversal
+def test_tree_preorder(simple_tree):
+    actual = simple_tree.pre_order_traverse()
+    print(actual)
+    expected = ["spam", "eggs", "hash"]
+    assert actual == expected
+
+# Can successfully return a collection from an inorder traversal
+def test_tree_inorder(simple_tree):
+    actual = simple_tree.in_order_traverse()
+    print(actual)
+    expected = ["eggs", "spam", "hash"]
+    assert actual == expected
+
+# Can successfully return a collection from a postorder traversal
+def test_tree_postorder(simple_tree):
+    actual = simple_tree.post_order_traverse()
+    print(actual)
+    expected = ["eggs", "hash", "spam"]
+    assert actual == expected
+
+@pytest.fixture
+def simple_tree():
+    tree = BinaryTree()
+    tree.root = Node("spam", left=Node("eggs"), right=Node("hash"))
+    return tree
--- python/trees/trees.py
+++ python/trees/trees.py
@@ -9,6 +9,42 @@ class BinaryTree():
     def __init__(self, root=None):
         self.root = root
 
+    def pre_order_traverse(self):
+
+        def _traversal(root, result = []):
+            result.append(root.value)
+            if root.left:
+                _traversal(root.left, result)
+            if root.right:
+                _traversal(root.right, result)
+            return result
+
+        return _traversal(self.root)
+
+    def in_order_traverse(self):
+
+        def _traversal(root, result = []):
+            if root.left:
+                _traversal(root.left, result)
+            result.append(root.value)
+            if root.right:
+                _traversal(root.right, result)
+            return result
+
+        return _traversal(self.root)
+
+    def post_order_traverse(self):
+
+        def _traversal(root, result = []):
+            if root.left:
+                _traversal(root.left, result)
+            if root.right:
+                _traversal(root.right, result)
+            result.append(root.value)
+            return result
+
+        return _traversal(self.root)
+
 
 class BinarySearchTree():
     pass
