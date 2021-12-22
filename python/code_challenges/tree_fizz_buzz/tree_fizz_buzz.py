b201b07fcfed9d3536b60116acdc6084d342c829
Joe Streifel <josephstreifel@gmail.com>
2021-12-22 14:49:43 -0800

    sets up for challenge, creates k_ary tree and node classes, creates first test

---
+++ python/code_challenges/tree_fizz_buzz/tree_fizz_buzz.py
@@ -0,0 +1,11 @@
+class KNode():
+    def __init__(self, value = None):
+        self.value = value
+        self.children = []
+
+class k_ary_tree():
+    def __init__(self):
+        self.root = None
+
+def fizz_buzz_tree(tree):
+    pass
---
+++ python/tests/test_tree_fizz_buzz.py
@@ -0,0 +1,20 @@
+from code_challenges.tree_fizz_buzz.tree_fizz_buzz import k_ary_tree, KNode, fizz_buzz_tree
+import pytest
+
+def test_tree_fizz_buzz_small(small_k_ary):
+    output_tree = fizz_buzz_tree(small_k_ary)
+    assert output_tree.root.value == '7'
+    val_children = [child.value for child in output_tree.children]
+    assert val_children == ['fizz', 'fizzbuzz', 'buzz']
+
+
+@pytest.fixture
+def small_k_ary():
+    tree = k_ary_tree()
+    tree.root = KNode(7)
+
+    children = [KNode(3), KNode(15), KNode(5)]
+    for child in children:
+        tree.root.children.append(child)
+
+    return tree
