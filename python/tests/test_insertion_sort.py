fec00b6356a9c873564e15ae4a5e3af286863a33
Joe Streifel <josephstreifel@gmail.com>
2022-01-05 16:28:34 -0800

    adds tests

--- python/tests/test_insertion_sort.py
+++ python/tests/test_insertion_sort.py
@@ -1,1 +1,29 @@
 from code_challenges.insertion_sort.insertion_sort import insertion_sort
+
+
+def test_sort_normal():
+    test_list = [6, 2, 9, 11, 0, 22, 17]
+    insertion_sort(test_list)
+
+    assert test_list == [0, 2, 6, 9, 11, 17, 22]
+
+
+def test_sort_one_item():
+    test_list = [7]
+    insertion_sort(test_list)
+
+    assert test_list == [7]
+
+
+def test_sort_empty_list():
+    test_list = []
+    insertion_sort(test_list)
+
+    assert test_list == []
+
+
+def test_sort_already_sorted():
+    test_list = [0, 2, 6, 9, 11, 17, 22]
+    insertion_sort(test_list)
+
+    assert test_list == [0, 2, 6, 9, 11, 17, 22]
