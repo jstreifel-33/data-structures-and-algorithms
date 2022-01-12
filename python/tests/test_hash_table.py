765dc4b89338e597baca95a7e203888633cebe9d
Joe Streifel <josephstreifel@gmail.com>
2022-01-11 16:37:00 -0800

    completes testing

--- python/tests/test_hash_table.py
+++ python/tests/test_hash_table.py
@@ -21,3 +21,40 @@ def test_get_key_not_in_table():
     ht.add("cat", "meow")
 
     assert ht.get("dog") is None
+
+
+def test_key_collision():
+    ht = HashTable()
+
+    ht.add("cat", "meow")
+    ht.add("tac", "woem")
+
+    #sanity check
+    assert ht.hash("cat") == ht.hash("tac")
+
+    #actual test
+    assert ht.get("cat") == "meow"
+    assert ht.get("tac") == "woem"
+
+
+def test_retrieve_from_collision():
+    ht = HashTable()
+
+    ht.add("dingo", "dog thing")
+    ht.add("doing", "things and stuff")
+    ht.add("gindo", "made up word")
+
+    assert ht.get("dingo") == "dog thing"
+    assert ht.get("doing") == "things and stuff"
+    assert ht.get("gindo") == "made up word"
+
+
+def test_hash_ranges():
+    ht = HashTable()
+
+    #test a bunch of hashes
+    assert 0 <= ht.hash("kittycat") <= 1023
+    assert 0 <= ht.hash("Charles Darwin") <= 1023
+    assert 0 <= ht.hash("Empire State Building") <= 1023
+    assert 0 <= ht.hash("The Literal Entire City of Albuquerque") <= 1023
+    assert 0 <= ht.hash("A really long string that I need for even more testing to make sure the number definitely rolls over instead of going out of range") <= 1023
