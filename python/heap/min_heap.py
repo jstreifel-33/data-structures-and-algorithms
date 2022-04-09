b7e7c9756e13da998c6bf3122439b146597d5e60
Joe Streifel <josephstreifel@gmail.com>
2022-04-09 12:49:25 -0700

    more helpers, heapify_up method

--- python/heap/min_heap.py
+++ python/heap/min_heap.py
@@ -10,6 +10,7 @@ class min_heap_list:
     def __init__(self):
         self.h = []
 
+    # retrieve parent, child indices
     def get_parent(self, idx):
         return (idx - 1) // 2
 
@@ -19,5 +20,27 @@ class min_heap_list:
     def get_right_child(self, idx):
         return 2 * idx + 2
 
+    # check for parent, children
+    def has_parent(self, idx):
+        return self.get_parent(idx) > 0
+
+    def has_left_child(self, idx):
+        return self.get_left_child(idx) < len(self.h)
+
+    def has_right_child(self, idx):
+        return self.get_right_child(idx) < len(self.h)
+
+    # swap two values in heap
     def swap(self, idx, idx_2):
         self.h[idx], self.h[idx_2] = self.h[idx_2], self.h[idx]
+
+    # maintain heap structure
+    def heapify_up(self):
+        idx = len(self.h) - 1
+        while self.has_parent(idx):
+            parent = self.get_parent(idx)
+            if self.h[idx] < self.h[parent]:
+                self.swap(idx, parent)
+                idx = parent
+                continue
+            break
