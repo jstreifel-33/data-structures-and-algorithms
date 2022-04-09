ebe0d176b454f198b28a9149a36903799be16e6a
Joe Streifel <josephstreifel@gmail.com>
2022-04-09 16:43:08 -0700

    implements maxheap usign heapq

--- python/heap/use_heapq.py
+++ python/heap/use_heapq.py
@@ -16,3 +16,28 @@ class minHeap:
 
     def __len__(self):
         return len(self.h)
+
+
+class maxHeapObj:
+    def __init__(self, val):
+        self.val = val
+
+    def __lt__(self, other):
+        return self.val > other.val
+
+    def __eq__(self, other):
+        return self.val == other.val
+
+    def __str__(self):
+        return str(self.val)
+
+
+class maxHeap(minHeap):
+    def add(self, val):
+        heapq.heappush(self.h, maxHeapObj(val))
+
+    def pop(self, val):
+        heapq.heappop(self.h, maxHeapObj(val))
+
+    def __getitem__(self, i):
+        return self.h[i].val
