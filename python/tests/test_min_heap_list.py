b673c060e1f708b17451b11bd653fcd4136a02f0
Joe Streifel <josephstreifel@gmail.com>
2022-04-09 14:17:05 -0700

    ensures heap maintains order, peek works, duplicates don't break heap

--- python/tests/test_min_heap_list.py
+++ python/tests/test_min_heap_list.py
@@ -22,3 +22,31 @@ def test_heap_multi():
     assert heap.pop() == 1
     assert heap.pop() == 2
     assert heap.pop() == 3
+
+
+def test_heap_unordered():
+    heap = min_heap_list()
+    heap.add(2)
+    heap.add(1)
+    heap.add(3)
+    assert heap.pop() == 1
+    assert heap.pop() == 2
+    assert heap.pop() == 3
+
+
+def test_heap_peek():
+    heap = min_heap_list()
+    values = [4, 6, 2, 7, 3, 5]
+
+    for value in values:
+        heap.add(value)
+
+    assert heap.peek() == 2
+
+
+def test_same_priority():
+    heap = min_heap_list()
+    heap.add(2)
+    heap.add(3)
+    heap.add(2)
+    assert heap.peek() == 2
